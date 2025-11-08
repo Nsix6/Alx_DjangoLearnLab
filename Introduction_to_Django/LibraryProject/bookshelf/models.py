daf577f01e267c5f7346cd38a8c5c70a9e62d8e2
Nsix6 <nsikakmbre@gmail.com>
2025-11-08 21:03:06 +0100

    customised and registerd add with admin privileges

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse 
+from django.db.models import UniqueConstraint 
 
 # Create your models here.
 
@@ -6,9 +8,18 @@ class Book(models.Model):
     """
     Defines a Book model with title, author, and publication year fields.
     """
-    title  = models.CharField(max_length=200)
+    title  = models.CharField(max_length=200, unique=True, help_text="Title of the book")
     author = models.CharField(max_length=100)
+    summary = models.TextField(blank=True, null=True, max_length=1000, help_text="Brief summary of the book",)
     publication_year = models.IntegerField()
+
+    class Meta:
+        """
+        Meta class for Book model to define constraints.
+        """
+        constraints = [
+            UniqueConstraint(fields=['title'], name='unique_book_title', violation_error_message="A book with this title already exists.")
+        ]
 
     def __str__(self):
         """
@@ -17,3 +28,7 @@ class Book(models.Model):
         :param self: Description
         """
         return f"{self.title} by {self.author} ({self.publication_year})"
+    
+    def get_absolute_url(self):
+        """Returns the URL to access a detail record for this book."""
+        return reverse('book-detail', args=[str(self.id)])
