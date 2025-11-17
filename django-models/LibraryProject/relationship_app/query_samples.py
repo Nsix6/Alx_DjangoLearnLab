51b622cb61f8c261e6d4e7d428e4058764d02eb3
Nsix6 <nsikakmbre@gmail.com>
2025-11-17 22:34:47 +0100

    django-models

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,4 +1,4 @@
-from relationship_app.models import Author, Book, Library, Librarian
+#from relationship_app.models import Author, Book, Library, Librarian
 
 def books_by_author(author_name):
     author = Author.objects.get(name=author_name)
