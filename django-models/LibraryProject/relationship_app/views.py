05c624bb475daeae2a6c6e3fc13649049e10e1b7
Nsix6 <nsikakmbre@gmail.com>
2025-11-18 13:26:56 +0100

    implemented templates for creatd views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from relationship_app.models import Book, Library
+from django.http import HttpResponse
 
 def book_list(request):
     """View to list all books titles with their authors."""
-    books = Book.objects.select_related('author').all()
-    context = {'books': books}
-    return render(request, 'relationship_app/book_list.html', context)
+    books = Book.objects.all()
+    output = ""
+    for book in books:
+        output += f"{book.title} - {book.author.name}\n"
+
+    return HttpResponse(output, content_type="text/plain")
 
 
 from django.views.generic import DetailView
