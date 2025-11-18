76b1f239dbbf328ecdb681dd4a0fe487f0e29476
Nsix6 <nsikakmbre@gmail.com>
2025-11-18 13:03:12 +0100

    implemented templates for creatd views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
+    path('books/', views.book_list, name='book_list'),
+    path('library/<int:pk>/', views.BookListView.as_view(), name='library_detail'),
 ]
