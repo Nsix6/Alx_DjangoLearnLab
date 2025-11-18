57223d6af72572d9790e11a8cac4b850e872148e
Nsix6 <nsikakmbre@gmail.com>
2025-11-18 20:33:17 +0100

    user auth & auth templates set

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -22,5 +22,23 @@ class LibraryDetailView(DetailView):
         return context
     
 
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.shortcuts import redirect
+from django.views.generic import CreateView
+
+
+class Register(CreateView):
+    """User registration view"""
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/register.html"
+
+
+    def form_valid(self, form):
+        user = form.save()
+        print("Saved user:", user)
+        return redirect('login')
+
 
 
