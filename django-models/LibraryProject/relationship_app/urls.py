714be9a3b0bccd3046a652be1af4a7c96858c08e
Nsix6 <nsikakmbre@gmail.com>
2025-11-19 04:21:04 +0100

    user auth & auth templates set

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
-from django.contrib.auth.views import LogoutView, LoginView
-from .views import list_books, LibraryDetailView, Register
-urlpatterns = [
-    path('books/', list_books, name='book_list'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path("register/", Register.as_view(), name="register"),
-    path("login/", LoginView.as_view(
-        template_name="relationship_app/login.html"
-    ), name="login"),
+from django.contrib.auth import views as auth_views
+from . import views
 
-    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
+urlpatterns = [
+    path('books/', views.list_books, name='book_list'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('register/', views.Register.as_view(), name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name="relationship_app/login.html"), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name="relationship_app/logout.html"), name='logout'),
 ]
