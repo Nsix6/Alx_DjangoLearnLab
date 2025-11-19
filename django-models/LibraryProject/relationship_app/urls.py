0cb12a54c77a7c4a796f560af1c5b59a39ae3d30
Nsix6 <nsikakmbre@gmail.com>
2025-11-19 19:32:36 +0100

    user auth & auth templates set

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from . import views
 
-register = views.Register.as_view()
 
 urlpatterns = [
     path('books/', views.list_books, name='book_list'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-     path('register/', register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name="relationship_app/login.html"), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name="relationship_app/logout.html"), name='logout'),
+
+
+    path("register/", views.register, name="register"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -22,26 +22,47 @@ class LibraryDetailView(DetailView):
         return context
     
 
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.contrib.auth import login, authenticate
-from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+
+def register(request):
+    """User Reistration View"""
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+
+            user = authenticate(
+                username=user.username,
+                password=request.POST.get("password1")
+            )
+
+            if user:
+                login(request, user)
+                return redirect('book_list')
+    
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form}) 
 
 
-class Register(CreateView):
-    """User registration view"""
-    form_class = UserCreationForm
-    success_url = reverse_lazy("login")
-    template_name = "relationship_app/login.html"
-
-    def form_valid(self, form):
-        response = super().form_valid(form)
-        user = authenticate(username=form.cleaned_data['username'],
-                            password=form.cleaned_data['password1'])
-        if user is not None:
-            login(self.request, user)
-        return response
+def login_view(request):
+    """User Login View"""
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('book_list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'relationship_app/login.html', {'form': form})
 
 
-
-
+def logout_view(request):
+    """Loogut view"""
+    if request.method == "POST":
+        logout(request)
+        return redirect('login')
+    return render(request, 'relationship_app/logout.html')
